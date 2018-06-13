463ff2352d9464e0e83aafa5b0fc495bdc5b7bd6
Shaun Underwood <shaun@dotfive.co.uk>
2018-06-13 08:12:32 +0200

    * Added ui-router and two routes

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,39 @@
 import angular from 'angular';
+import '@uirouter/angularjs';
+import './controllers/BlogEntry.controller';
+import './services/Blog.service';
 
 import '../style/app.css';
 
-let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
-};
+function routing($stateProvider) {
 
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
+	let defaultState = {
+		name: 'bloglist',
+		url: '',
+		template: require('./templates/blog-list.html')
+	};
+
+	let newBlogEntryState = {
+		name: 'newblog',
+		url: '/new',
+		template: require('./templates/blog-edit.html')
+	};
+
+	$stateProvider.state(defaultState);
+	$stateProvider.state(newBlogEntryState);
 }
+
+let app = () => {
+	return {
+		template: require('./app.html'),
+	}
+};
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, ['BlogEntry'])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+angular.module(MODULE_NAME, ['BlogEntry', 'ui.router'])
+	.directive('app', app)
+	.config(routing)
+;
 
 export default MODULE_NAME;
